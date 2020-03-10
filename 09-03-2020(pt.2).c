3d2d78e6c29a9fed6d6c21f914023a760d6f0531
Guilherme Moreno Cancelinha <42576214+cancelinha@users.noreply.github.com>
2020-03-09 21:43:58 -0300

    Add files via upload

---
+++ 09-03-2020(pt.2).c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+main(){
+char nome_completo[50];
+int ano_nascimento;
+char sexo[2];
+int resultado;
+
+/*printf("Boa noite.\n Digite seu Nome Completo:");
+gets(nome_completo);
+printf("Por ultimo me informe o seu sexo: M ou F? ");
+gets(sexo);
+printf("Agora me informe seu ano de Nascimento:");
+scanf("%d", &ano_nascimento);
+printf("Muito obrigado pelas informacoes\n Seu nome eh %c, sua idade eh %d eh seu sexo e %c", nome_completo, ano_nascimento, sexo);
+*/
+int n1, n2, n3,n4;
+printf("Digite o 1o numero:");
+scanf("%d",&n1);
+printf("Digte o 2o numero:");
+scanf("%d",&n2);
+printf("Digite o 3o numero:");
+scanf("%d",&n3);
+printf("Digite o 4o numero:");
+scanf("%d",&n4);
+
+resultado =  n1+n2+n3+n4;
+
+printf("Soma : %d", resultado);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
